c7fe5940694cb1999834a15ae77eb9b3f1b3d1ea
TAL <talush38@gmail.com>
2015-05-09 14:10:34 +0300

    aa

--- app/src/main/java/com/MyCookBook/Fragment/SearchFragment.java
+++ app/src/main/java/com/MyCookBook/Fragment/SearchFragment.java
@@ -6,6 +6,8 @@ import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RelativeLayout;
 
 import com.example.mycookbook.mycookbook.R;
 
@@ -14,6 +16,7 @@ import com.example.mycookbook.mycookbook.R;
  */
 public class SearchFragment extends Fragment {
 
+    private RelativeLayout searchLayout;
 
     public SearchFragment() {
 
@@ -24,5 +27,26 @@ public class SearchFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.activity_search_fragment, container , false);
         return rootView;
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.RBall:
+                if (checked)
+                    // Pirates are the best
+                    break;
+            case R.id.RBusers:
+                if (checked)
+                    // Ninjas rule
+                    break;
+            case R.id.RBwebsite:
+                if (checked)
+                    // Ninjas rule
+                    break;
+        }
     }
 }
