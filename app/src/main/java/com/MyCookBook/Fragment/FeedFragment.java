ed1db8aee2ad5330904a8fa3258f931414b5f48c
TAL <talush38@gmail.com>
2015-05-12 20:57:57 +0300

    aa

--- app/src/main/java/com/MyCookBook/Fragment/FeedFragment.java
+++ app/src/main/java/com/MyCookBook/Fragment/FeedFragment.java
@@ -8,13 +8,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
+import android.widget.ScrollView;
 import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.example.mycookbook.mycookbook.R;
 
 public class FeedFragment extends Fragment {
 
     AutoCompleteTextView myAutoComplete;
+    TableLayout tbLayout;
 
     public FeedFragment() {
 
@@ -38,6 +44,56 @@ public class FeedFragment extends Fragment {
                 R.array.ingridient_type_array,
                 android.R.layout.simple_list_item_1);
         myAutoComplete.setAdapter(adapter);
+
+        tbLayout = (TableLayout) rootView.findViewById(R.id.tbFeed);
+        //tbLayout.setTextDirection(View.LAYOUT_DIRECTION_RTL);
+        //tbLayout.setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
+
+        for (int i = 0; i < 4; i++) {
+
+            TextView tvRecipe = new TextView(getActivity().getBaseContext());
+            ImageView ivRecipePhoto = new ImageView(getActivity().getBaseContext());
+
+            TableRow tr = new TableRow(getActivity().getBaseContext());
+            TableRow.LayoutParams trLP = new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT,
+                    TableRow.LayoutParams.WRAP_CONTENT);
+            tr.setLayoutParams(trLP);
+
+            // Handle recipe text
+            tvRecipe.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+            tvRecipe.setMinimumWidth(350);
+            tvRecipe.setText("המתכון של טל\n\n" + i + "\n" + "רכיבים:\nשוקולד\nסוכר");
+            //tvRecipe.setTextDirection(View.LAYOUT_DIRECTION_RTL);
+
+            // Handle recipe image
+            ivRecipePhoto.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+            ivRecipePhoto.setMaxWidth(350);
+            ivRecipePhoto.setMaxHeight(350);
+            ivRecipePhoto.setAdjustViewBounds(true);
+            ivRecipePhoto.setImageResource(R.drawable.com_facebook_profile_picture_blank_square);
+
+            // Add to layOut
+            tr.addView(tvRecipe);
+            tr.addView(ivRecipePhoto);
+
+            tbLayout.addView(tr);
+        }
+
+        /*TextView tvIngredients = new TextView(getActivity().getBaseContext());
+        TableRow tr2 = new TableRow(getActivity().getBaseContext());
+        TableRow.LayoutParams trLP2 = new TableRow.LayoutParams(TableRow.LayoutParams.FILL_PARENT,
+                TableRow.LayoutParams.WRAP_CONTENT);
+        tr.setLayoutParams(trLP2);
+
+        // Handle recipe text
+        tvIngredients.setLayoutParams(new TableRow.LayoutParams(TableRow.LayoutParams.MATCH_PARENT, TableRow.LayoutParams.WRAP_CONTENT));
+        tvIngredients.setMinimumWidth(350);
+        tvIngredients.setText("רכיבים:\nשוקולד\nסוכר");
+        //tvIngredients.setTextDirection(View.LAYOUT_DIRECTION_RTL);
+
+        // Add to layOut
+        tr2.addView(tvIngredients);
+        tbLayout.addView(tr2); */
 
         return rootView;
     }
