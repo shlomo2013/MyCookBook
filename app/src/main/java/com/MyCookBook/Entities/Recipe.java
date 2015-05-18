6b01f5b89e7b1a72aecc62ac896a3823d0d5d4e4
shlomo2013 <maillaba@gmail.com>
2015-05-19 00:12:57 +0300

    Recipe Upgrade

--- app/src/main/java/com/MyCookBook/Entities/Recipe.java
+++ app/src/main/java/com/MyCookBook/Entities/Recipe.java
@@ -1,6 +1,7 @@
 package com.MyCookBook.Entities;
 
 import com.parse.ParseClassName;
+import com.parse.ParseFile;
 import com.parse.ParseObject;
 import com.parse.ParseRelation;
 
@@ -16,18 +17,32 @@ public class Recipe  extends ParseObject {
     public static final String SubCategory = "SubCategory";
     public static final String Preparation = "Preparation";
     public static final String Groceries = "Groceries";
+    public static final String DishType = "DishType";
+    public static final String Difficulty = "Difficulty";
+    public static final String KitchenType = "KitchenType";
+    public static final String Diet = "Diet";
+    public static final String Vegetarian = "Vegetarian";
+    public static final String Vegan = "Vegan";
+    public static final String RecipePic = "RecipePic.jpg";
     ArrayList<Grocery> groceries = new ArrayList<Grocery>();
 
 
     public Recipe() {
     }
 
-    public void initRecipe(String name,String category,String subCategory,String preparation){
+    public void initRecipe(String name,String category,String subCategory,String preparation,String dishType,
+                           String difficulty,String kitchenType,String diet,String vegetarian,String vegan){
         this.put(Name, name);
         this.put(Category, category);
         this.put(SubCategory, subCategory);
         this.put(Preparation, preparation);
         this.put(Groceries, groceries);
+        this.put(DishType, dishType);
+        this.put(Difficulty, difficulty);
+        this.put(KitchenType, kitchenType);
+        this.put(Diet, diet);
+        this.put(Vegetarian, vegetarian);
+        this.put(Vegan, vegan);
         this.saveInBackground();
     }
 
@@ -46,5 +61,11 @@ public class Recipe  extends ParseObject {
         this.saveInBackground();
     }
 
-
+    public void savePic(byte[] data){
+        //byte[] data = "Working at Parse is great!".getBytes();
+        ParseFile file = new ParseFile(RecipePic, data);
+        file.saveInBackground();
+        this.put(RecipePic,file);
+        this.saveInBackground();
+    }
 }
