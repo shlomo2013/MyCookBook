670584504d006aad5b0e6f76a94459c4d8066eaa
shlomo2013 <maillaba@gmail.com>
2015-06-06 20:51:15 +0300

    getAlbunById

--- app/src/main/java/com/MyCookBook/Fragment/MainActivity.java
+++ app/src/main/java/com/MyCookBook/Fragment/MainActivity.java
@@ -47,6 +47,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setMyUserId(savedInstanceState);
+
         //Queries.updateMyUser(myUserId);
 
         /**
--- app/src/main/java/com/example/mycookbook/mycookbook/Queries.java
+++ app/src/main/java/com/example/mycookbook/mycookbook/Queries.java
@@ -135,6 +135,23 @@ public class Queries {
         return retRecipe;
     }
 
+    public static Album getAlbumById(String objectId) {
+        ParseQuery<ParseObject> recQuery = ParseQuery.getQuery("Album");
+        recQuery.whereEqualTo("objectId", objectId);
+        List<ParseObject> recList = null;
+        Album retAlbum = null;
+        try {
+            recList = recQuery.find();
+        } catch (Exception e) {
+            Log.d("getRecipeById Err", "cannot find Recipe by objId");
+        }
+        if (recList.size() != 0) {
+            retAlbum = (Album) recList.get(0);
+        }
+
+        return retAlbum;
+    }
+
     public static ArrayList<Grocery> getGroceriesById(ArrayList<String> objectIds) {
         ParseQuery<ParseObject> recQuery = ParseQuery.getQuery("Grocery");
         recQuery.whereContainedIn("objectId", objectIds);
