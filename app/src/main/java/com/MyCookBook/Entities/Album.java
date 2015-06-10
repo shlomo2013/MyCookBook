87721725f300bd3a418b851a10cc1b1d1f6159ef
shlomo2013 <maillaba@gmail.com>
2015-06-10 21:32:25 +0300

    Album new C'tor and another functions

--- app/src/main/java/com/MyCookBook/Entities/Album.java
+++ app/src/main/java/com/MyCookBook/Entities/Album.java
@@ -25,6 +25,8 @@ public class Album  extends ParseObject {
     public static final String Pic="Pic";
     public static final String Recipes="Recipes";
     public static final String Users="Users";
+    public static final String AlbumType="AlbumType";
+    public static final String Description="Description";
     public static final String CreatedBy="CreatedBy";
 
 
@@ -35,10 +37,22 @@ public class Album  extends ParseObject {
         this.saveInBackground();
     }
 
+    public void removeRecipe(Recipe recipe){
+        ParseRelation<ParseObject> relation = this.getRelation(Recipes);
+        relation.remove(recipe);
+        this.saveInBackground();
+    }
+
     /*Add user to album*/
     public void addUser(User user){
         ParseRelation<ParseObject> relation = this.getRelation(Users);
         relation.add(user);
+        this.saveInBackground();
+    }
+
+    public void removeUser(User user){
+        ParseRelation<ParseObject> relation = this.getRelation(Users);
+        relation.remove(user);
         this.saveInBackground();
     }
 
@@ -97,12 +111,63 @@ public class Album  extends ParseObject {
         this.setAlbumName(AlbumName);
         this.setUserCreatedBy(userCreated);
     }
+
+    public Album(String albumName, String albumType,String description, User userCreated,ArrayList<User> permittedUser, Bitmap pic)
+    {
+        this.putOrDefault(Name,albumName);
+        this.putOrDefault(AlbumType, albumType);
+        this.putOrDefault(Description, description);
+        this.putParseOrDefault(CreatedBy, userCreated);
+        this.relateUserToAlbum(permittedUser);
+        if(pic!=null) {
+            ParseFile file = new ParseFile("AlbumPic.jpeg", bitmapToByteArray(pic));
+            file.saveInBackground();
+            this.put(Pic, file);
+        }
+    }
+
+    public void relateUserToAlbum(ArrayList<User> users){
+        if(users==null){
+            return;
+        }
+        ParseRelation<ParseObject> relation = this.getRelation(Users);
+        for(User currUser:users){
+            relation.add(currUser);
+        }
+    }
+
+    public void saveAlbum(){
+        try{
+            this.save();
+        }catch(Exception e){
+            Log.d("Cannot save Album: ",e.getMessage());
+        }
+    }
+
     public String getAlbumName()
     {
         return getString(Name);
     }
     public void setAlbumName(String value) {
         putOrDefault(Name,value);
+        this.saveInBackground();
+    }
+
+
+    public String getAlbumType()
+    {
+        return getString(AlbumType);
+    }
+    public void setAlbumType(String value) {
+        putOrDefault(AlbumType,value);
+        this.saveInBackground();
+    }
+    public String getDescription()
+    {
+        return getString(Description);
+    }
+    public void setDescription(String value) {
+        putOrDefault(Description,value);
         this.saveInBackground();
     }
 
@@ -121,6 +186,14 @@ public class Album  extends ParseObject {
             this.put(att,"");
         }else{
             this.put(att,value);
+        }
+    }
+
+    private void putParseOrDefault(String att,ParseObject object){
+        if(object==null){
+            this.put(att,"");
+        }else{
+            this.put(att,object);
         }
     }
 
